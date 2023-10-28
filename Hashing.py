bf62cd457eb61e7258673180de0975902051207e
Lagan Verma <72144796+laganverma@users.noreply.github.com>
2023-10-28 17:27:41 +0530

    Update Hashing.py
    Adding GUI

--- Hashing.py
+++ Hashing.py
@@ -1,5 +1,8 @@
 import hashlib
 import time
+import json
+import tkinter as tk
+from tkinter import messagebox
 
 class Block:
     def __init__(self, index, previous_hash, timestamp, data, hash):
@@ -22,19 +25,60 @@ def calculate_hash(index, previous_hash, timestamp, data):
     value = str(index) + previous_hash + str(timestamp) + data
     return hashlib.sha256(value.encode('utf-8')).hexdigest()
 
+def save_blockchain(blockchain):
+    with open('blockchain.json', 'w') as file:
+        json.dump([vars(block) for block in blockchain], file)
+
+def load_blockchain():
+    try:
+        with open('blockchain.json', 'r') as file:
+            data = json.load(file)
+            return [Block(block['index'], block['previous_hash'], block['timestamp'], block['data'], block['hash']) for block in data]
+    except FileNotFoundError:
+        return []
+
+def validate_blockchain(blockchain):
+    for i in range(1, len(blockchain)):
+        if blockchain[i].previous_hash != blockchain[i-1].hash:
+            return False
+    return True
+
+def add_block():
+    new_block_data = entry.get()
+    if not new_block_data:
+        messagebox.showerror("Error", "Please enter block data")
+        return
+
+    new_block = create_new_block(blockchain[-1], new_block_data)
+    blockchain.append(new_block)
+    save_blockchain(blockchain)
+
+    entry.delete(0, tk.END)
+    label.config(text="Block #{} added to the blockchain".format(new_block.index))
+
+def show_blockchain():
+    chain = "\n".join("Block #{}: {}".format(block.index, block.data) for block in blockchain)
+    messagebox.showinfo("Blockchain", chain)
+
 def main():
-    blockchain = [create_genesis_block()]
-    previous_block = blockchain[0]
+    blockchain = load_blockchain()
 
-    num_of_blocks_to_add = 20
+    root = tk.Tk()
+    root.title("Blockchain GUI")
 
-    for i in range(0, num_of_blocks_to_add):
-        new_block_data = "Hey! I'm block #" + str(i)
-        new_block = create_new_block(previous_block, new_block_data)
-        blockchain.append(new_block)
-        previous_block = new_block
-        print("Block #{} has been added to the blockchain!".format(new_block.index))
-        print("Hash: {}\n".format(new_block.hash))
+    label = tk.Label(root, text="Enter block data:")
+    label.pack()
+
+    entry = tk.Entry(root, width=30)
+    entry.pack()
+
+    add_button = tk.Button(root, text="Add Block", command=add_block)
+    add_button.pack()
+
+    show_button = tk.Button(root, text="Show Blockchain", command=show_blockchain)
+    show_button.pack()
+
+    root.mainloop()
 
 if __name__ == '__main__':
     main()
